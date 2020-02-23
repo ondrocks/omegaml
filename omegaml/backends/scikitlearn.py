3317d29ce88878f793184a2d8ae22463e02004fa
Patrick <miraculixx@gmx.ch>
2020-02-23 11:23:24 +0100

    updates fixing broken tests

--- omegaml/backends/scikitlearn.py
+++ omegaml/backends/scikitlearn.py
@@ -3,71 +3,84 @@ from __future__ import absolute_import
 import glob
 import os
 import tempfile
-from shutil import rmtree
 from zipfile import ZipFile, ZIP_DEFLATED
 
 import datetime
 import joblib
 from mongoengine.fields import GridFSProxy
+from shutil import rmtree
 from sklearn.model_selection import GridSearchCV
 
 from omegaml.backends.basemodel import BaseModelBackend
 from omegaml.documents import MDREGISTRY
 from omegaml.util import reshaped, gsreshaped
-from hashlib import sha1
 
 # byte string
 _u8 = lambda t: t.encode('UTF-8', 'replace') if isinstance(t, str) else t
 
 
-class ScikitLearnBackend(BaseModelBackend):
-    """
-    OmegaML backend to use with ScikitLearn
-    """
-
-    def _package_model(self, model, filename):
+class ScikitLearnBackendV1(BaseModelBackend):
+    # kept to support legacy scikit learn model serializations prior to ~scikit learn v0.18
+    def _v1_package_model(self, model, filename):
         """
         Dumps a model using joblib and packages all of joblib files into a zip
         file
         """
-        joblib.dump(model, filename, protocol=4)
-        return filename
+        import joblib
+        lpath = tempfile.mkdtemp()
+        fname = os.path.basename(filename)
+        mklfname = os.path.join(lpath, fname)
+        zipfname = os.path.join(self.model_store.tmppath, fname)
+        joblib.dump(model, mklfname, protocol=4)
+        with ZipFile(zipfname, 'w', compression=ZIP_DEFLATED) as zipf:
+            for part in glob.glob(os.path.join(lpath, '*')):
+                zipf.write(part, os.path.basename(part))
+        rmtree(lpath)
+        return zipfname
 
-    def _extract_model(self, packagefname):
+    def _v1_extract_model(self, packagefname):
         """
         Loads a model using joblib from a zip file created with _package_model
         """
-        model = joblib.load(packagefname)
+        import joblib
+        lpath = tempfile.mkdtemp()
+        fname = os.path.basename(packagefname)
+        mklfname = os.path.join(lpath, fname)
+        with ZipFile(packagefname) as zipf:
+            zipf.extractall(lpath)
+        model = joblib.load(mklfname)
+        rmtree(lpath)
         return model
 
     def get_model(self, name, version=-1):
         """
         Retrieves a pre-stored model
         """
-        meta = self.model_store.metadata(name)
-        outf = meta.gridfile
-        packagefname = os.path.join(self.model_store.tmppath, outf.name + '.omm')
+        filename = self.model_store._get_obj_store_key(name, '.omm')
+        packagefname = os.path.join(self.model_store.tmppath, name)
         dirname = os.path.dirname(packagefname)
-        os.makedirs(dirname, exist_ok=True)
+        try:
+            os.makedirs(dirname)
+        except OSError:
+            # OSError is raised if path exists already
+            pass
+        outf = self.model_store.fs.get_version(filename, version=version)
         with open(packagefname, 'wb') as zipf:
             zipf.write(outf.read())
-        model = self._extract_model(packagefname)
+        model = self._v1_extract_model(packagefname)
         return model
 
     def put_model(self, obj, name, attributes=None):
         """
         Packages a model using joblib and stores in GridFS
         """
-        hasher = sha1()
-        hasher.update(_u8(self.model_store._get_obj_store_key(name, 'omm')))
-        filename = hasher.hexdigest()
-        zipfname = self._package_model(obj, filename)
+        zipfname = self._v1_package_model(obj, name)
         with open(zipfname, 'rb') as fzip:
             fileid = self.model_store.fs.put(
-                fzip, filename=filename)
-        gridfile = GridFSProxy(grid_id=fileid,
-                               db_alias='omega',
-                               collection_name=self.model_store.bucket)
+                fzip, filename=self.model_store._get_obj_store_key(name, 'omm'))
+            gridfile = GridFSProxy(grid_id=fileid,
+                                   db_alias='omega',
+                                   collection_name=self.model_store.bucket)
         return self.model_store._make_metadata(
             name=name,
             prefix=self.model_store.prefix,
@@ -76,8 +89,77 @@ class ScikitLearnBackend(BaseModelBackend):
             attributes=attributes,
             gridfile=gridfile).save()
 
+
+class ScikitLearnBackendV2(ScikitLearnBackendV1):
+    """
+    OmegaML backend to use with ScikitLearn
+    """
+    _backend_version_tag = '_om_backend_version'
+    _backend_version = '2'
+
+    def _v2_package_model(self, model, key):
+        """
+        Dumps a model using joblib and packages all of joblib files into a zip
+        file
+        """
+        packagefname = self._tmp_packagefn(key)
+        joblib.dump(model, packagefname, protocol=4)
+        return packagefname
+
+    def _v2_extract_model(self, infile, key):
+        """
+        Loads a model using joblib from a zip file created with _package_model
+        """
+        packagefname = self._tmp_packagefn(key)
+        with open(packagefname, 'wb') as pkgf:
+            pkgf.write(infile.read())
+        model = joblib.load(packagefname)
+        return model
+
+    def _tmp_packagefn(self, name):
+        filename = os.path.join(self.model_store.tmppath, name)
+        dirname = os.path.dirname(filename)
+        os.makedirs(dirname, exist_ok=True)
+        return filename
+
+    def get_model(self, name, version=-1):
+        """
+        Retrieves a pre-stored model
+        """
+
+        meta = self.model_store.metadata(name)
+        if self._backend_version_tag not in meta.kind_meta:
+            return super().get_model(name, version=version)
+        storekey = self.model_store.object_store_key(name, 'omm', hashed=True)
+        model = self._v2_extract_model(meta.gridfile, storekey)
+        return model
+
+    def put_model(self, obj, name, attributes=None, _kind_version=None):
+        """
+        Packages a model using joblib and stores in GridFS
+        """
+        if _kind_version and _kind_version != self._backend_version:
+            return super().put_model(obj, name, attributes=attributes)
+        storekey = self.model_store.object_store_key(name, 'omm', hashed=True)
+        packagefname = self._v2_package_model(obj, storekey)
+        gridfile = GridFSProxy(db_alias='omega',
+                               collection_name=self.model_store._fs_collection)
+        with open(packagefname, 'rb') as pkgf:
+            fileid = gridfile.put(pkgf, filename=storekey)
+        kind_meta = {
+            self._backend_version_tag: self._backend_version,
+        }
+        return self.model_store._make_metadata(
+            name=name,
+            prefix=self.model_store.prefix,
+            bucket=self.model_store.bucket,
+            kind=MDREGISTRY.SKLEARN_JOBLIB,
+            kind_meta=kind_meta,
+            attributes=attributes,
+            gridfile=gridfile).save()
+
     def predict(
-            self, modelname, Xname, rName=None, pure_python=True, **kwargs):
+          self, modelname, Xname, rName=None, pure_python=True, **kwargs):
         data = self.data_store.get(Xname)
         model = self.model_store.get(modelname)
         result = model.predict(reshaped(data), **kwargs)
@@ -89,7 +171,7 @@ class ScikitLearnBackend(BaseModelBackend):
         return result
 
     def predict_proba(
-            self, modelname, Xname, rName=None, pure_python=True, **kwargs):
+          self, modelname, Xname, rName=None, pure_python=True, **kwargs):
         data = self.data_store.get(Xname)
         model = self.model_store.get(modelname)
         result = model.predict_proba(reshaped(data), **kwargs)
@@ -122,7 +204,7 @@ class ScikitLearnBackend(BaseModelBackend):
         return meta
 
     def partial_fit(
-            self, modelname, Xname, Yname=None, pure_python=True, **kwargs):
+          self, modelname, Xname, Yname=None, pure_python=True, **kwargs):
         model = self.model_store.get(modelname)
         X, metaX = self.data_store.get(Xname), self.data_store.metadata(Xname)
         Y, metaY = None, None
@@ -144,8 +226,8 @@ class ScikitLearnBackend(BaseModelBackend):
         return meta
 
     def score(
-            self, modelname, Xname, Yname, rName=None, pure_python=True,
-            **kwargs):
+          self, modelname, Xname, Yname, rName=None, pure_python=True,
+          **kwargs):
         model = self.model_store.get(modelname)
         X = self.data_store.get(Xname)
         Y = self.data_store.get(Yname)
@@ -156,8 +238,8 @@ class ScikitLearnBackend(BaseModelBackend):
         return result
 
     def fit_transform(
-            self, modelname, Xname, Yname=None, rName=None, pure_python=True,
-            **kwargs):
+          self, modelname, Xname, Yname=None, rName=None, pure_python=True,
+          **kwargs):
         model = self.model_store.get(modelname)
         X, metaX = self.data_store.get(Xname), self.data_store.metadata(Xname)
         Y, metaY = None, None
@@ -232,3 +314,7 @@ class ScikitLearnBackend(BaseModelBackend):
         })
         meta.save()
         return meta
+
+
+class ScikitLearnBackend(ScikitLearnBackendV2):
+    pass
--- omegaml/tests/test_store.py
+++ omegaml/tests/test_store.py
@@ -50,9 +50,11 @@ class StoreTests(unittest.TestCase):
         # package locally
         backend = backends.ScikitLearnBackend(model_store=store,
                                               data_store=store)
-        zipfname = backend._package_model(lr, 'models/foo')
+        # v2 of the ScikitLearnBackend no longer supports testing these methods
+        # test put(), get() instead
+        zipfname = backend._v1_package_model(lr, 'models/foo')
         # load it, try predicting
-        lr2 = backend._extract_model(zipfname)
+        lr2 = backend._v1_extract_model(zipfname)
         self.assertIsInstance(lr2, LogisticRegression)
         result2 = lr2.predict(X)
         self.assertTrue((result == result2).all())
@@ -352,7 +354,7 @@ class StoreTests(unittest.TestCase):
         lr = LogisticRegression(solver='liblinear', multi_class='auto')
         lr.fit(X, Y)
         # store it remote
-        store.put(lr, 'foo')
+        store.put(lr, 'foo', _kind_version='1')
         # get it back as a zipfile
         lr2file = store.get('foo', force_python=True)
         contents = lr2file.read()
@@ -749,6 +751,27 @@ class StoreTests(unittest.TestCase):
         store.put('/tmp/testfile.txt', 'myfile')
         self.assertEqual(data.encode('utf-8'), store.get('myfile').read())
 
-
-
+    def test_bucket(self):
+        # test different buckets actually separate objects by the same name
+        # -- data
+        foo_store = OmegaStore(bucket='foo')
+        bar_store = OmegaStore(bucket='bar')
+        foo_store.register_backend(PythonRawFileBackend.KIND, PythonRawFileBackend)
+        bar_store.register_backend(PythonRawFileBackend.KIND, PythonRawFileBackend)
+        foo_data = {'foo': 'bar',
+                'bax': 'fox'}
+        bar_data = {'foo': 'bax',
+                    'bax': 'foz'}
+        foo_store.put(foo_data, 'data')
+        bar_store.put(bar_data, 'data')
+        self.assertEqual(foo_store.get('data')[0], foo_data)
+        self.assertEqual(bar_store.get('data')[0], bar_data)
+        # -- files
+        foo_data = "some data"
+        file_like = BytesIO(foo_data.encode('utf-8'))
+        foo_store.put(file_like, 'myfile')
+        bar_data = "some other data"
+        file_like = BytesIO(bar_data.encode('utf-8'))
+        bar_store.put(file_like, 'myfile')
+        self.assertNotEqual(foo_store.get('myfile').read(), bar_store.get('myfile').read())
 
